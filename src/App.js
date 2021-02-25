9b4e2bc0ef32686617ef54ac0abe224b0c39428b
kaibalya123 <kaibalyamohanty0@gmail.com>
2021-02-26 00:52:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App = ()=> {
+  return (
+    
+    <ChatEngine height="100vh" projectID="8f11c77c-72a2-4135-8d8f-e679e33083fd" userName="krishna" userSecret="1234"/>
+   
+
+   
+  );
+}
+
+export default App;
