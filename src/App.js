2f638f55927239dc12046d3b2344ca6c928c2d29
kaibalya123 <kaibalyamohanty0@gmail.com>
2021-02-27 23:51:36 +0530

    customization for chat feed is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './Component/ChatFeed'
 
 const App = ()=> {
   return (
     
-    <ChatEngine height="100vh" projectID="8f11c77c-72a2-4135-8d8f-e679e33083fd" userName="krishna" userSecret="1234"/>
+    <ChatEngine height="100vh" projectID="8f11c77c-72a2-4135-8d8f-e679e33083fd" userName="krishna" userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
    
 
    
